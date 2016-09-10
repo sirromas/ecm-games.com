329f4f7326aaa510b6824f32c3d5bfe4dfc8c2c3
sirromas <sirromas@gmail.com>
2016-09-10 15:34:22 +0300

    Views fixes

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -16,52 +16,17 @@
     <div class="ft-bt">
         <div id="footermenu">
             <div class="footer-img-center"></div>
-
-            <script type="text/javascript">
-
-                var width = $(window).width(), height = $(window).height();
-                console.log('Width: ' + width);
-                console.log('Height: ' + height);
-
-                if (width < 768) {
-                    console.log('Inside if ...');
-
-
-                }
-                else {
-                    console.log('Inside else ...');
-
-
-                }
-
-            </script>
-
             <?php echo "<div class='copyright'>Все права защищены<br />Wd-studio</div>"; ?>
-
-
             <div class="footer-img-right"></div>
         </div>
-        <!--div class="footer-b"></div-->
+        
 </footer>
 <script type="text/javascript">
-    jQuery(document).ready(function ($) {
-        /*
-         setTimeout(function () {
-         // Do something after 1 second 
-         $('html, body').animate({
-         scrollTop: $("#icon-menu").offset().top
-         }, 1000);
-         }, 2500);
-         */
-    });
-
-</script>
 
 <?php echo "<script src='http://" . $_SERVER['SERVER_NAME'] . '/assets/js/tablesorter/sorter.js' . "'></script>"; ?>
 
-<script type="text/javascript">
-
-    jQuery(document).ready(function ($) {
+    <script type = "text/javascript" >
+            jQuery(document).ready(function ($) {
         $("#icon-menu").on("click", function () {
             $(this).toggleClass("active");
             $("#menu ul").toggleClass("toggled");
@@ -72,41 +37,6 @@
         });
 
         $('[data-toggle="popover"]').popover({html: true});
-
-        $("#discount5").mouseover(function () {
-            $('#help5').show();
-        });
-        $("#discount5").mouseout(function () {
-            $('#help5').hide();
-        });
-
-        $("#discount4").mouseover(function () {
-            $('#help4').show();
-        });
-        $("#discount4").mouseout(function () {
-            $('#help4').hide();
-        });
-
-        $("#discount3").mouseover(function () {
-            $('#help3').show();
-        });
-        $("#discount3").mouseout(function () {
-            $('#help3').hide();
-        });
-
-        $("#discount2").mouseover(function () {
-            $('#help2').show();
-        });
-        $("#discount2").mouseout(function () {
-            $('#help2').hide();
-        });
-
-        $("#discount1").mouseover(function () {
-            $('#help1').show();
-        });
-        $("#discount1").mouseout(function () {
-            $('#help1').hide();
-        });
 
     });
 </script>
--- application/views/right_block_view.php
+++ application/views/right_block_view.php
@@ -18,24 +18,14 @@
                 <div id="minfin-informer-m1Fn-currency">Загружаем <a href="http://minfin.com.ua/currency/" target="_blank">курсы валют</a> от minfin.com.ua</a></div><script type="text/javascript">var iframe = '<ifra' + 'me width="159" height="120" fram' + 'eborder="0" src="http://informer.minfin.com.ua/gen/course/?color=yellow" vspace="0" scrolling="no" hspace="0" allowtransparency="true"style="width:159px;height:120px;ove' + 'rflow:hidden;"></iframe>';
                     var cl = 'minfin-informer-m1Fn-currency';
                     document.getElementById(cl).innerHTML = iframe;</script><noscript><img src="http://informer.minfin.com.ua/gen/img.png" width="1" height="1" alt="minfin.com.ua: курсы валют" title="Курс валют" border="0" /></noscript>
-                <!-- Minfin.com.ua currency informer 159x120 yellow-->
 
             </div>
             <br>
-
-            <!--
-            <div style='text-align: center;'>    
-            Мы в контакте <br>
-            <a href='https://vk.com/ecm_games' target="_blank" style="color: #000000;font-size: 14px;text-decoration: none">https://vk.com/ecm_games</a>
-            </div>
-            -->
 
             <div id="vk_groups"></div>
             <script type="text/javascript">
                 VK.Widgets.Group("vk_groups", {mode: 0, width: "180", height: "400"}, 102469496);
             </script>
-
-
         </div>                
     </div>  
 </div>
